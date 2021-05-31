363022c8c0abb0ab132275756c7e8cf3d9bb6bdd
Wesley Liu <wesley@naos.finance>
2021-05-31 21:18:17 +0800

    lock contracts solidity version

--- contracts/Formation.sol
+++ contracts/Formation.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.6.12;
+pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
 import {Math} from "@openzeppelin/contracts/math/Math.sol";
