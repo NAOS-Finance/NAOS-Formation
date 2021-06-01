cac6796865e5a8f8308213585e919c314508bbaa
Wesley Liu <wesley@naos.finance>
2021-06-01 18:15:03 +0800

    replace chainlink deprecated function

--- contracts/interfaces/IChainlink.sol
+++ contracts/interfaces/IChainlink.sol
@@ -2,5 +2,5 @@
 pragma solidity 0.6.12;
 
 interface IChainlink {
-  function latestAnswer() external view returns (int256);
+  function latestRoundData() external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 }
