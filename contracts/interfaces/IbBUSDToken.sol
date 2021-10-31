6ffb0e50bb54717b0010d1460eb66f8602a70b1e
x64x48 <x64x48@protonmail.com>
2021-10-31 20:26:28 +0800

    Accrue interest value for alpaca vault

--- contracts/interfaces/IbBUSDToken.sol
+++ contracts/interfaces/IbBUSDToken.sol
@@ -4,6 +4,12 @@ pragma solidity 0.6.12;
 import {IDetailedERC20} from "./IDetailedERC20.sol";
 
 interface IbBUSDToken is IDetailedERC20 {
+    function vaultDebtVal() external view returns (uint256);
+
+    function lastAccrueTime() external view returns (uint256);
+
+    function reservePool() external view returns (uint256);
+
     function deposit(uint256) external;
 
     function withdraw(uint256) external;
